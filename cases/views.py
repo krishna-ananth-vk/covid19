e0a9497d7cc1a8e577e5d119cc15d2d35b9af7cd
the-code-builder <krishnaananthvk@gmail.com>
2020-07-15 15:43:31 +0530

    edit page updated

--- cases/views.py
+++ cases/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
+import os
 
 from django.http import HttpResponse,Http404
 from .models import District
 
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import storage
+
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# cred = credentials.Certificate(os.path.join(BASE_DIR, 'test-ce84a8a94eca.json'))
+# firebase_admin.initialize_app(cred, {
+#     'storageBucket': 'test-6615.appspot.com'
+# })
+
+# bucket = storage.bucket()
+
+
+# blob = bucket.blob("rsz_20161114_123947-1.jpg")
+# print(vars(blob))
+
+# blob.download_to_filename("C:\\Users\\ASUS\\Documents\\covid19\\covid19")
+
 # Create your views here.
 
 def index(request):
+    
 
 
     d = District.objects.order_by('id')
@@ -26,9 +48,16 @@ def districtwise(request,did):
     return render(request, 'district.html', context)
 
 def edit(request,did):
-    dis = District.objects.get(pk=did)
-    context = {'d': dis}
-    return render(request, 'edit.html', context)
+
+    if request.user.is_authenticated:
+        print("test")
+
+        dis = District.objects.get(pk=did)
+        context = {'d': dis}
+        return render(request, 'edit.html', context)
+    else:
+        return render(request, 'authentication_needed.html')
+        # raise Http404("Not logged in")
     
 
 def updated(request,did):
